012e2499a6e877beb15ce8f9ba0427fd0ef9d7c8
Overkillas <kaikepn2@gmail.com>
2024-10-07 17:45:15 -0300

    Adicionado a função de get medicos

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,13 +3,13 @@ package med.voll.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 //migration com flyway
 @RestController
@@ -19,12 +19,21 @@ public class MedicoController {
     @Autowired
     private MedicoRepository repository;
 
-    //vai buscar o http
+    //vai falar qual verbo do protocolo http vai ser chamado (nesse caso: POST)
     @PostMapping
     //Insert no sql
     @Transactional
     public String cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
         return "medico cadastrado com sucesso!";
+    }
+
+    //vai falar qual verbo do protocolo http vai ser chamado (nesse caso: GET)
+    @GetMapping
+    public List<DadosListagemMedico> listarMedicos() {
+        //repository.findAll() [encontra e retorna lista de medicos
+        //stream().map(DadosListagemMedico::new) [converte a lista de medico para dadosListagemMedico]
+        //.toList() [converte para lista]
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
